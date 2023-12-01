1aa7dc051b5e3f887ba9ec97dcb3add6609b205a
SlappyBacon <soggichips@gmail.com>
2023-12-01 13:22:34 -0500

    New Additions
    DiskItemCollection added.  It's a addition to the DiskItem tool, which allows for objects to be paired with a guid.
    
    DiskItemLibrary added.  It's an addition to the DiskItemCollection which allows only one instance of an object to be 'borrowed' at a time.  That object must be returned to the library before another instance can be accessed.

---
+++ DiskItemCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Pico.Guid;
+
+namespace Pico.Files;
+
+class DiskItemCollection<T>
+{
+    string _rootDirectory;
+    public string RootDirectory => _rootDirectory;
+    const string FileExtention = ".json";
+
+    public DiskItemCollection(string rootDirectory)
+    {
+        _rootDirectory = Path.GetFullPath(rootDirectory);
+    }
+    public string Add(in T item)
+    {
+        var guid = GuidGenerator.Next();
+        var saved = Save(guid, item);
+        if (!saved) return null;
+        return guid;
+    }
+    public bool Save(string guid, in T item) => DiskItem.Save(GetFilePath(guid), item);
+    public bool Load(string guid, out T item) => DiskItem.Load(GetFilePath(guid), out item);
+    public bool Delete(string guid)
+    {
+        var filePath = GetFilePath(guid);
+        if (!File.Exists(filePath)) return false;
+
+        File.Delete(filePath);
+        return true;
+
+    }
+
+    public bool LoadFind(Func<T, bool> determinant, out T item) => DiskItem.LoadFind(RootDirectory, determinant, out item);
+    public List<T> LoadFindAll(Func<T, bool> determinant) => DiskItem.LoadFindAll(RootDirectory, determinant);
+
+    public bool GuidExists(string guid)
+    {
+        var filePath = GetFilePath(guid);
+        return File.Exists(filePath);
+    }
+    string GetFilePath(string guid)
+    {
+        //New StringBuilder (root.length + guid.length + extention.length)
+        var builder = new StringBuilder(RootDirectory.Length + guid.Length * FileExtention.Length);
+        builder.Append(RootDirectory);
+        builder.Append(guid);
+        builder.Append(FileExtention);
+        return builder.ToString();
+    }
+}
---
+++ DiskItemLibrary.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+namespace Pico.Files;
+class DiskItemLibrary<T> : IDisposable
+{
+    object _padlock = new object();
+    DiskItemCollection<T> _collection;
+    List<string> _borrowedGuids;
+    DiskItemCollection<T> Collection => _collection;
+    List<string> BorrowedGuids => _borrowedGuids;
+
+    bool _isDisposing = false;
+    public bool IsDisposing => _isDisposing;
+
+    public DiskItemLibrary(string rootDirectory)
+    {
+        _collection = new DiskItemCollection<T>(rootDirectory);
+        _borrowedGuids = new List<string>();
+    }
+
+    public string Add(in T item)
+    {
+        if (IsDisposing) return null;
+        lock (_padlock)
+        {
+            return Collection.Add(item);
+        }
+    }
+    public bool TryCopy(string guid, out T item)
+    {
+        //Does not "borrow" so you don't need to return the copy.
+        if (IsDisposing)
+        {
+            item = default;
+            return false;
+        }
+        lock (_padlock)
+        {
+            if (IsBorrowed(guid))
+            {
+                item = default;
+                return false;
+            }
+
+            var loaded = Collection.Load(guid, out item);
+            if (!loaded) return false;
+
+            return true;
+        }
+    }
+    public bool TryBorrow(string guid, out T item)
+    {
+        if (IsDisposing)
+        {
+            item = default;
+            return false;
+        }
+        lock (_padlock)
+        {
+            if (IsBorrowed(guid))
+            {
+                item = default;
+                return false;
+            }
+
+            var loaded = Collection.Load(guid, out item);
+            if (!loaded) return false;
+
+            BorrowedGuids.Add(guid);
+
+            return true;
+        }
+    }
+    public bool TryReturn(string guid, in T item)
+    {
+        lock (_padlock)
+        {
+            if (!IsBorrowed(guid))
+            {
+                return false;
+            }
+
+            var saved = Collection.Save(guid, item);
+            if (!saved) return false;
+
+            BorrowedGuids.Remove(guid);
+
+            return true;
+        } 
+    }
+    public bool TryDelete(string guid)
+    {
+        if (IsDisposing) return false;
+        lock (_padlock)
+        {
+            if (IsBorrowed(guid)) return false;
+            return Collection.Delete(guid);
+        }
+    }
+    public bool GuidExists(string guid)
+    {
+        lock (_padlock)
+        {
+            return Collection.GuidExists(guid);
+        }
+    }
+    public bool IsBorrowed(string guid)
+    {
+        lock (_padlock)
+        {
+            return BorrowedGuids.Contains(guid);
+        }
+    }
+
+    
+    public void Dispose()
+    {
+        _isDisposing = true;
+        while (true)
+        {
+            if (BorrowedGuids.Count < 1) break;
+            Thread.Sleep(1);
+        }
+    }
+}
