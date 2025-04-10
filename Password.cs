b84b7dce579a1d345e8dc310de3e28e8c018db11
Slappy_Bacon_ <soggichips@gmail.com>
2025-04-10 19:09:58 -0400

    Password.cs Added.
    A class that is used to store a string semi-safely as a hash.  You cannot read the string back out, however, you may input a string to check if the hashes match.  The object is also serializable.

---
+++ Password.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
+namespace Pico.Credentials.Login
+{
+    struct Password
+    {
+        [JsonProperty]
+        byte[] _hash;
+        [JsonIgnore]
+        byte[] Hash => _hash;
+
+        public Password(string text) => Set(text);
+        void Set(string text) => _hash = ToHash(text);
+
+        public bool Try(string text)
+        {
+            var textHash = ToHash(text);
+            var areSame = ArraysAreSame(textHash, Hash);
+            return areSame;
+        }
+
+        public bool Change(string oldValue, string newValue)
+        {
+            var confirmed = Try(oldValue);
+            if (!confirmed) return false;
+            Set(newValue);
+            return true;
+        }
+
+        byte[] ToHash(string text)
+        {
+            //Null check
+            if (text == null) text = "";
+            //Get text bytes
+            var textBytes = Encoding.UTF8.GetBytes(text);
+            //Encrypt?
+            //Hash
+            var sha = SHA512.Create();
+            var hash = sha.ComputeHash(textBytes);
+            sha.Dispose();
+            return hash;
+        }
+
+        bool ArraysAreSame(in byte[] arr1, in byte[] arr2)
+        {
+            if (arr1 == null || arr2 == null) return false;
+
+            //Both not null
+            if (arr1.Length != arr2.Length) return false;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i]) return false;
+            }
+
+            return true;
+        }
+    }
+}
