49a742308c52234c1701d0e43f58080666396eba
Slappy_Bacon_ <soggichips@gmail.com>
2025-04-10 19:07:42 -0400

    JsonTools.cs Added.
    Print -> Serializes an object then prints it to console.
    Rebuild -> Serializes then deserializes an object to test if integrity is maintained.

---
+++ JsonTools.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Pico.Json;
+
+public static class JsonTools
+{
+    public static void Print<T>(T obj)
+    {
+        var json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        Console.WriteLine(json);
+    }
+    public static void Rebuild<T>(ref T obj)
+    {
+        var json = JsonConvert.SerializeObject(obj);
+        var disposeMe = obj as IDisposable;
+        disposeMe?.Dispose();
+        obj = JsonConvert.DeserializeObject<T>(json);
+    }
+}
