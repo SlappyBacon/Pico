0643fb9da106c5aaaa47d8089fe6ba1102f1d544
Slappy_Bacon_ <soggichips@gmail.com>
2025-05-13 19:27:17 -0400

    Added Service.cs
    Service.cs is an wrapper class for creating services.  It contains a main Task and some common functionality like Start(), Stop(), and its status.

---
+++ Service.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    abstract class Service : IDisposable
+    {
+        CancellationTokenSource mainCts_ = null;
+        CancellationTokenSource MainCts => mainCts_;
+
+        Task mainTask_ = null;
+        Task MainTask => mainTask_;
+
+        public bool IsRunning => MainTask != null && !MainTask.IsCompleted;
+
+        bool isStarting_ = false;
+        public bool IsStarting => isStarting_;
+
+        bool isStopping_ = false;
+        public bool IsStopping => isStopping_;
+
+        protected abstract Task Main(CancellationToken cancelToken);
+
+        public async Task<bool> Start()
+        {
+            if (IsRunning) return false;
+            if (IsStopping) return false;
+            isStarting_ = true;
+            mainCts_ = new CancellationTokenSource();
+            mainTask_ = Main(MainCts.Token);
+            isStarting_ = false;
+            return true;
+        }
+        public async Task<bool> Stop()
+        {
+            if (!IsRunning) return false;
+            if (IsStarting) return false;
+            isStopping_ = true;
+            MainCts.Cancel();
+            MainTask.Wait();
+            MainTask.Dispose();
+            MainCts.Dispose();
+            isStopping_ = false;
+            return true;
+        }
+
+        public async void Dispose()
+        {
+            await Stop();
+        }
+    }
+}
