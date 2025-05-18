b60b7f63865b969a6dfa6d51c88864000e38ecbd
Slappy_Bacon_ <TuckerBrinkman778@gmail.com>
2025-05-18 08:21:35 -0400

    Update Service.cs

--- Service.cs
+++ Service.cs
@@ -1,55 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestApp
+﻿namespace Pico.Services;
+abstract class Service : IDisposable
 {
-    abstract class Service : IDisposable
+    CancellationTokenSource mainCts_ = null;
+    CancellationTokenSource MainCts => mainCts_;
+
+    Task mainTask_ = null;
+    Task MainTask => mainTask_;
+
+    public bool IsRunning => MainTask != null && !MainTask.IsCompleted;
+
+    bool isStarting_ = false;
+    public bool IsStarting => isStarting_;
+
+    bool isStopping_ = false;
+    public bool IsStopping => isStopping_;
+
+    protected abstract Task Main(CancellationToken cancelToken);
+
+    public bool Start()
     {
-        CancellationTokenSource mainCts_ = null;
-        CancellationTokenSource MainCts => mainCts_;
+        if (IsRunning) return false;
+        if (IsStopping) return false;
+        isStarting_ = true;
 
-        Task mainTask_ = null;
-        Task MainTask => mainTask_;
+        mainCts_ = new CancellationTokenSource();
+        mainTask_ = Main(MainCts.Token);
 
-        public bool IsRunning => MainTask != null && !MainTask.IsCompleted;
+        isStarting_ = false;
+        return true;
+    }
+    public bool Stop()
+    {
+        if (!IsRunning) return false;
+        if (IsStarting) return false;
+        isStopping_ = true;
 
-        bool isStarting_ = false;
-        public bool IsStarting => isStarting_;
+        MainCts.Cancel();
+        MainTask.Wait();
 
-        bool isStopping_ = false;
-        public bool IsStopping => isStopping_;
+        MainTask.Dispose();
+        MainCts.Dispose();
 
-        protected abstract Task Main(CancellationToken cancelToken);
+        isStopping_ = false;
+        return true;
+    }
 
-        public async Task<bool> Start()
-        {
-            if (IsRunning) return false;
-            if (IsStopping) return false;
-            isStarting_ = true;
-            mainCts_ = new CancellationTokenSource();
-            mainTask_ = Main(MainCts.Token);
-            isStarting_ = false;
-            return true;
-        }
-        public async Task<bool> Stop()
-        {
-            if (!IsRunning) return false;
-            if (IsStarting) return false;
-            isStopping_ = true;
-            MainCts.Cancel();
-            MainTask.Wait();
-            MainTask.Dispose();
-            MainCts.Dispose();
-            isStopping_ = false;
-            return true;
-        }
-
-        public async void Dispose()
-        {
-            await Stop();
-        }
+    public void Dispose()
+    {
+        Stop();
     }
 }
